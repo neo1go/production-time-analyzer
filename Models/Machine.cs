cba210f151c688aa4fae483e15364f428b97c8cc
Gordon H <neo1go@gmx.de>
2026-05-06 15:01:27 +0200

    Initialized minimum models for TimeEntry, Product and Machine

--- Models/Machine.cs
+++ Models/Machine.cs
@@ -2,5 +2,8 @@
 {
     public class Machine
     {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public string? Type { get; set; }
     }
 }
--- Models/TimeEntry.cs
+++ Models/TimeEntry.cs
@@ -2,5 +2,11 @@
 {
     public class TimeEntry
     {
+        public int Id { get; set; }
+        public int ProductId { get; set; }
+        public int MachineId { get; set; }
+        public DateTime Start {  get; set; }
+        public DateTime End { get; set; }
+        public string? Status { get; set; }
     }
 }
